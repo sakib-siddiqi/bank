add4f33f850afe217e9e0154f6d1fff296112144
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-08-16 11:27:19 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 let email=document.querySelector('#emailInput');
 let password=document.querySelector('#passInput');
 let submit=document.querySelector('#submitBtn');
+// login
 submit.addEventListener('click',function(){
     if(email.value=="sakib@gmail.com" && password.value=="password"){
        location.href="banking.html";
+       clearFuild([email,password]);
     }else{
         console.log("wrong")
     }
 })
+// clear fuild
+function clearFuild(fuild){
+    fuild.forEach(function(e){e.value=""});
+}
---
+++ js/banking.js
@@ -0,0 +1,48 @@
+// calculation
+let dipoBtn = document.querySelector("#dipoBtn");
+let diposite = document.querySelector("#dipo");
+let dipoMoney = document.querySelector(".dipoMoney");
+let withdrowBtn = document.querySelector("#withdrowBtn");
+let withdrow = document.querySelector("#withdrow");
+let withdrowMoney = document.querySelector(".withdrowMoney");
+let balanceMoney = document.querySelector(".balanceMoney");
+//clear fuild
+ function clearFuild(fuild){
+    fuild.forEach(function(e){e.value=""});
+}
+// diposite
+dipoBtn.addEventListener("click", function () {
+    let balance = parseFloat(balanceMoney.innerText);
+    let newBalance = parseFloat(diposite.value);
+    let oldBalance = parseFloat(dipoMoney.innerText);
+    if (newBalance < 0) {
+        clearFuild([diposite])
+        diposite.style.borderColor = '#FF4848';
+    }
+    else {
+        let totalBalance = balance + newBalance;
+        let totalDipo = newBalance + oldBalance;
+        dipoMoney.innerText = totalDipo; 
+        balanceMoney.innerText=totalBalance;
+        diposite.style.borderColor = '#00ADB5';
+        clearFuild([diposite])
+    }
+})
+withdrowBtn.addEventListener("click", function () {
+    let newBalance = parseFloat(withdrow.value);
+    let oldBalance=parseFloat(withdrowMoney.innerText)
+    let balance = parseFloat(balanceMoney.innerText);
+    if (newBalance>0) {
+        let totalwithdrow = oldBalance + newBalance;
+        let totalBalance = balance - newBalance;
+        withdrowMoney.innerText = totalwithdrow;
+        balanceMoney.innerText = totalBalance;
+        withdrow.style.borderColor = '#00ADB5';
+        clearFuild([withdrow])
+    }
+    else {
+        withdrow.style.borderColor = '#FF4848';
+        clearFuild([withdrow])
+    }
+
+})
